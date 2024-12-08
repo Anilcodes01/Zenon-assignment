e62b69e7de371f2c0cec9a25a18807d975e4902c
Anil Pal <anilcodes01@gmail.com>
2024-12-08 10:42:59 +0530

    360 viewer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Poppins } from '@next/font/google';
 import Appbar from "./components/Appbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'], // Select desired font weights
